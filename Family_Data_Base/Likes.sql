f888e08721bdb8a09907304c85c22307ae9bb28a
MohammedAbuissa <mohammed.abuissa@outlook.com>
2015-12-20 07:27:25 +0200

    Images become a byte arrays, edited like table bug, changed the primary key of posts to be timestamp isntead of int

--- Family_Data_Base/Likes.sql
+++ Family_Data_Base/Likes.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Likes]
 (
 	[User_Id] INT NOT NULL,
-	[Post_Id] INT NOT NULL,
-	constraint PK_Likes primary key (User_Id, Post_Id),
-	constraint FK_LikePost foreign key (User_Id,Post_Id) references Posts(Post_Id,User_Id)
+	[Post_Id] DATETIME NOT NULL,
+	[Like_Owner_Id] INT NOT NULL, 
+    constraint PK_Likes primary key (User_Id, Post_Id),
+	constraint FK_LikePost foreign key (User_Id,Post_Id) references Posts(User_Id,Time)
 )
--- Family_Data_Base/Posts.sql
+++ Family_Data_Base/Posts.sql
@@ -1,10 +1,9 @@
 ﻿CREATE TABLE [dbo].[Posts]
 (
-	[Post_Id] INT NOT NULL,
 	[User_Id] INT FOREIGN KEY REFERENCES Users(User_Id),
 	[Caption] text,
-	[Image] INT,
-	[Time] timestamp NOT NULL,
+	[Image] VARBINARY(MAX),
+	[Time] DATETIME NOT NULL,
 	[Private_Public] bit default(0),
-	constraint PK_Post Primary key(Post_Id,User_Id)
+	constraint PK_Post Primary key(User_Id,Time)
 )
