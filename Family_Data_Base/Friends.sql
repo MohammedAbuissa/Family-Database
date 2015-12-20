ad1045525a4b40e9c831b8c4af27f1882abc8821
MohammedAbuissa <mohammed.abuissa@outlook.com>
2015-12-20 22:01:01 +0200

    added accepted attribute to friends

--- Family_Data_Base/Friends.sql
+++ Family_Data_Base/Friends.sql
@@ -2,5 +2,6 @@
 (
 	[User_Id] INT NOT NULL foreign key references Users (User_Id),
 	[Friend_Id] INT NOT NULL foreign key references Users (User_Id),
-	constraint pk_friend primary key (User_Id, Friend_Id)
+	[Accepted] BIT NOT NULL, 
+    constraint pk_friend primary key (User_Id, Friend_Id)
 )
